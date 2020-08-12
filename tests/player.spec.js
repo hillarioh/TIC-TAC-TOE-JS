55bed13e8533166ce33c4ae7734679275d5eee37
hillarioh <okerioh@gmail.com>
2020-08-12 12:38:33 +0300

    add player tests

---
+++ tests/player.spec.js
@@ -0,0 +1,10 @@
+import player from "../Js/player";
+
+
+const players = player.Players();
+
+it("return an array with current player first",()=>
+expect(players.changePlayer(2)).toEqual(['X','O']))
+
+it("returns X if player name matches player 1",()=>
+expect(players.playerSign('X')).toBe('X'))
